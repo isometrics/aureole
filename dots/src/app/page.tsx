ab65ece3657ef81e284fd6c3977990b292a08d48
yyualvin <136134023+yyualvin@users.noreply.github.com>
2025-07-23 11:48:16 -0400

    chore: create the center card

---
+++ dots/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen p-4">
+      <div className="bg-white rounded-2xl h-full min-h-[calc(100vh-2rem)] shadow-sm border border-gray-100">
+        {/* Your content goes here */}
+      </div>
+    </div>
+  );
+}
