6c6daededbd6f2b39b592869ecb9362d8da748b0
yyualvin <136134023+yyualvin@users.noreply.github.com>
2025-07-28 18:22:07 -0400

    chore: starting to implement function calling

---
+++ backend/commits_over_time.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import logging
+import plotly.express as px
+from utils.graph_utils import get_graph_time_values, color_seq
+from celery_app import commits_query as cmq
+from utils.job_utils import nodata_graph
+import time
+import cache_manager.cache_facade as cf
+
+def commits_over_time_tool(repolist, interval="M"):
+    graph = commits_over_time_graph(repolist, interval)
+    title = "Commits Over Time"
+    description = "Visualizes the number of commits added to the project.\n Commits are counted relative to a user-selected time window."
+    return graph, title, description
+
+def commits_over_time_graph(repolist, interval="M"):
+    # wait for data to asynchronously download and become available.
+    while not_cached := cf.get_uncached(func_name=cmq.__name__, repolist=repolist):
+        logging.warning(f"COMMITS_OVER_TIME_VIZ - WAITING ON DATA TO BECOME AVAILABLE")
+        time.sleep(0.5)
+
+    # data ready.
+    start = time.perf_counter()
+    logging.warning("COMMITS_OVER_TIME_VIZ - START")
+
+    # GET ALL DATA FROM POSTGRES CACHE
+    df = cf.retrieve_from_cache(
+        tablename=cmq.__name__,
+        repolist=repolist,
+    )
+
+    # test if there is data
+    if df.empty:
+        logging.warning("COMMITS OVER TIME - NO DATA AVAILABLE")
+        return nodata_graph
+
+    # function for all data pre processing
+    df_created = process_data(df, interval)
+
+    fig = create_figure(df_created, interval)
+
+    logging.warning(f"COMMITS_OVER_TIME_VIZ - END - {time.perf_counter() - start}")
+    return fig
+
+
+def process_data(df: pd.DataFrame, interval):
+    # convert to datetime objects with consistent column name
+    # incoming value should be a posix integer.
+    df["author_date"] = pd.to_datetime(df["author_date"], utc=True)
+    df.rename(columns={"author_date": "created_at"}, inplace=True)
+
+    # variable to slice on to handle weekly period edge case
+    period_slice = None
+    if interval == "W":
+        # this is to slice the extra period information that comes with the weekly case
+        period_slice = 10
+
+    # get the count of commits in the desired interval in pandas period format, sort index to order entries
+    df_created = (
+        df.groupby(by=df.created_at.dt.to_period(interval))["commit_hash"]
+        .nunique()
+        .reset_index()
+        .rename(columns={"created_at": "Date"})
+    )
+
+    # converts date column to a datetime object, converts to string first to handle period information
+    # the period slice is to handle weekly corner case
+    df_created["Date"] = pd.to_datetime(df_created["Date"].astype(str).str[:period_slice])
+
+    return df_created
+
+
+def create_figure(df_created: pd.DataFrame, interval):
+    # time values for graph
+    x_r, x_name, hover, period = get_graph_time_values(interval)
+
+    # graph geration
+    fig = px.bar(
+        df_created,
+        x="Date",
+        y="commit_hash",
+        range_x=x_r,
+        labels={"x": x_name, "y": "Commits"},
+        color_discrete_sequence=[color_seq[3]],
+    )
+    fig.update_traces(hovertemplate=hover + "<br>Commits: %{y}<br>")
+    fig.update_xaxes(
+        showgrid=True,
+        ticklabelmode="period",
+        dtick=period,
+        rangeslider_yaxis_rangemode="match",
+        range=x_r,
+    )
+    fig.update_layout(
+        xaxis_title=x_name,
+        yaxis_title="Number of Commits",
+        margin_b=40,
+        margin_r=20,
+        font=dict(size=14),
+    )
+
+    return fig
